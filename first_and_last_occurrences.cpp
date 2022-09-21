808c74f12036c6d985ddbcecb80ae0f4cb60e344
BhavyaGumber <79220494+BhavyaGumber@users.noreply.github.com>
2022-09-22 01:24:11 +0530

    Create first_and_last_occurrences.cpp

---
+++ first_and_last_occurrences.cpp
@@ -0,0 +1,19 @@
+vector<int> find(int arr[], int n , int x )
+{
+    // code here
+    int a1=-1;
+    int a2=-1;
+    for(int i=0;i<n;i++){
+        if(arr[i]==x){
+            a1=i;
+            break;
+        }
+    }
+    for(int i=n-1;i>=0;i--){
+        if(arr[i]==x){
+            a2=i;
+            break;
+        }
+    }
+    return {a1,a2};
+}
